4fcc2d9fb21e25b16437dde462ab6e5ed64af445
MichałGawędzki <michal5890@gmail.com>
2020-04-02 19:42:18 +0200

    separated histograms

--- biometria_przetwarzanie_obrazow/HistogramWindow.xaml.cs
+++ biometria_przetwarzanie_obrazow/HistogramWindow.xaml.cs
@@ -26,74 +26,79 @@ namespace biometria_przetwarzanie_obrazow {
 		public string[] Labels { get; set; }
 		public Func<double, string> YFormatter { get; set; }
 
-        int[] r;
-        int[] g;
-        int[] b;
+		int[] r;
+		int[] g;
+		int[] b;
 
 		public HistogramWindow(Bitmap sourceImage) {
 			InitializeComponent();
 
-            Bitmap img = sourceImage;
+			Bitmap img = sourceImage;
 
-            r = getRarray(img);
+			fillRgbArrays(img);
 
-            SeriesCollection = new SeriesCollection
-            {
-                new LineSeries
-                {
-                    Title = "Series 1",
-                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
-                },
-                new LineSeries
-                {
-                    Title = "Series 2",
-                    Values = new ChartValues<int> (r.ToArray()),
-                    PointGeometry = null
-                },
-                new LineSeries
-                {
-                    Title = "Series 3",
-                    Values = new ChartValues<double> { 4,2,7,2,7 },
-                    PointGeometry = DefaultGeometries.Square,
-                    PointGeometrySize = 15
-                }
-            };
-            var stringList = new List<String>();
-            for (int i = 0; i < 256; i++) {
-                stringList.Add(i.ToString());
-            }
-            Labels = stringList.ToArray();
+			SeriesCollection = new SeriesCollection
+			{
+				new LineSeries
+				{
+					Title = "Red",
+					Values = new ChartValues<int> (r.ToArray()),
+					PointGeometry = null,
+					Stroke = System.Windows.Media.Brushes.Red,
+				},
+				new LineSeries
+				{
+					Title = "Green",
+					Values = new ChartValues<int> (g.ToArray()),
+					PointGeometry = null,
+					Stroke = System.Windows.Media.Brushes.Green
+				},
+				new LineSeries
+				{
+					Title = "Blue",
+					Values = new ChartValues<int> (b.ToArray()),
+					PointGeometry = null,
+					Stroke = System.Windows.Media.Brushes.Blue
+				}
+			};
+			var stringList = new List<String>();
+			for (int i = 0; i < 256; i++) {
+				stringList.Add(i.ToString());
+			}
+			Labels = stringList.ToArray();
 
-            //Labels = new[] {"Jan", "Feb", "Mar", "Apr", "May"};
-            YFormatter = value => value.ToString("C");
- 
-            //modifying the series collection will animate and update the chart
-            SeriesCollection.Add(new LineSeries
-            {
-                Title = "Series 4",
-                Values = new ChartValues<double> {5, 3, 2, 4},
-                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
-                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
-                PointGeometrySize = 50,
-                PointForeground = System.Windows.Media.Brushes.Gray
-            });
- 
-            //modifying any series values will also animate and update the chart
-            SeriesCollection[3].Values.Add(5d);
- 
-            DataContext = this;
-        }
+			//Labels = new[] {"Jan", "Feb", "Mar", "Apr", "May"};
+			YFormatter = value => value.ToString("C");
 
-        public int[] getRarray(Bitmap bitmap) {
+			//modifying the series collection will animate and update the chart
+			//SeriesCollection.Add(new LineSeries {
+			//	Title = "Series 4",
+			//	Values = new ChartValues<double> { 5, 3, 2, 4 },
+			//	LineSmoothness = 0, //0: straight lines, 1: really smooth lines
+			//	PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
+			//	PointGeometrySize = 50,
+			//	PointForeground = System.Windows.Media.Brushes.Gray
+			//});
 
-            int[] arr = new int[256];
-            for(int i = 0; i < bitmap.Width; i++) {
-                for(int j = 0; j < bitmap.Height; j++) {
-                    System.Drawing.Color color = bitmap.GetPixel(i, j);
-                    arr[color.R]++;
-                }
-            }
-            return arr;
-        }
+			////modifying any series values will also animate and update the chart
+			//SeriesCollection[3].Values.Add(5d);
+
+			DataContext = this;
+		}
+
+		public void fillRgbArrays(Bitmap bitmap) {
+
+			r = new int[256];
+			g = new int[256];
+			b = new int[256];
+			for (int i = 0; i < bitmap.Width; i++) {
+				for (int j = 0; j < bitmap.Height; j++) {
+					System.Drawing.Color color = bitmap.GetPixel(i, j);
+					r[color.R]++;
+					g[color.G]++;
+					b[color.B]++;
+				}
+			}
+		}
 	}
 }
